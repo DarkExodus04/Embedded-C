c14611ea118811edbba99a599f2c0a172a1f7b71
Nathan Noronha <50819342+DarkExodus04@users.noreply.github.com>
2024-03-11 23:13:17 -0400

    Create swap-two-bits-in-a-given-integer.c

---
+++ swap-two-bits-in-a-given-integer.c
@@ -0,0 +1,8 @@
+// C program to swap bits in an integer
+#include<stdio.h>
+
+// follow up 
+// optimised version
+//thing i can ask interviwer
+// different methonds to solve the same question 
+// my logic written in plain english 
